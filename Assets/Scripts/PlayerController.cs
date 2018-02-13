c6b8295e72dacd757a59f3a3e21a58c3be895afc
Unknown <l.bourdrel@laposte.net>
2018-02-13 22:29:53 +0100

    Set up audio

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("Mouse")]
+    public float hDeadZone;
+    public float vDeadZone;
+    public float hSpeed;
+    public float vSpeed;
+    public Camera cam;
+
+    float yaw;
+    float pitch;
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            yaw = hSpeed * Input.GetAxis("Mouse X");
+            CamRotation();
+        }
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            pitch = vSpeed * Input.GetAxis("Mouse Y");
+            CamRotation();
+        }
+    }
+
+    void CamRotation()
+    {
+        cam.transform.Rotate(new Vector3(pitch, yaw, 0.0f));
+    }
+   
+}
