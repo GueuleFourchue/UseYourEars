f0edccbe34b82399642082dfcb200c3a1962e7a4
Unknown <l.bourdrel@laposte.net>
2018-02-19 09:09:48 +0100

    Set Up ANDROID

---
+++ Assets/Scripts/GyroCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCamera : MonoBehaviour
+{
+	GameObject camParent;
+
+	void Start()
+	{
+		camParent = new GameObject ("camParent");
+		camParent.transform.position = this.transform.position;
+		this.transform.parent = camParent.transform;
+		Input.gyro.enabled = true;
+	}
+
+	void Update()
+	{
+		camParent.transform.Rotate (0, -Input.gyro.rotationRateUnbiased.y, 0);
+		this.transform.Rotate (-Input.gyro.rotationRateUnbiased.x, 0, 0);
+	}
+
+}
